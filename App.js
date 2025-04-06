f0dc8794ef0ee3e1426b8c4b7a00adc89ff528e1
carmen <marreola9@ivytech.edu>
2025-04-06 00:16:43 -0400

    todolist2

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Task from "./components/Task";
+import { KeyboardAvoidingView } from "react-native-web";
 
 export default function App() {
+const [task, setTask] = useState();
+const [taskItems, setTaskItems] = useState([]);
+
+const handleAddTask = () => {
+  Keyboard.dismiss();
+ setTaskItems([...taskItems, task])
+ setTask(null);
+}
+const completeTask = (index) => {
+  let itemsCopy = [...taskItems];
+  itemsCopy.splice(index, 1);
+  setTaskItems(itemsCopy);
+}
+
   return (
     <View style={styles.container}>
       {/* Today's Tasks */}
@@ -11,10 +26,34 @@ export default function App() {
 
         <View styles={styles.items}>
           {/*this is where the task will go!*/}
+          {
+            taskItems.map((item, index) => {
+              return (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item} />
+
+              </TouchableOpacity>
+              )
+              <Task key={index} text={item} />
+          })
+        }
           <Task text={"Task 1"} />
           <Task text={"Task 2"} />
         </View>
       </View>
+      // Write a task
+      <KeyboardAvoidingView
+        behavior={Plataform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextImput style={styles.imput} placeholder={"Write a task"} value={task} onChageText={text => setTask(text)} />
+
+        <TouchableOpacity onPress={() => handleAddTask()}>>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -35,4 +74,32 @@ const styles = StyleSheet.create({
   items: {
     marginTop: 30,
   },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  imput: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: "FFF",
+    borderRadius: 60,
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#FFF",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderColor: "#C0C0C0",
+    borderWidth: 1,
+  },
+  addText: {},
 });
